600780c477dd102c19c1d67b74e7b17d1ddfe918
Paloma Levy <palomalevy@gmail.com>
2025-07-24 09:54:31 -0700

    [Core feature] Added styling to nav buttons

--- client/src/components/HomePageItems/LeftSideNav.jsx
+++ client/src/components/HomePageItems/LeftSideNav.jsx
@@ -1,17 +1,33 @@
-import React from 'react'
+import React, {useState}  from 'react'
 import {data} from '../../temp-data/temp-data';
+import ShinyText from '../Styling/ShinyText';
 import { Link } from 'react-router-dom';
 
 const LeftSideNav = ({ user, setMyPosts, setDiscover }) => {
+    const [clickedDiscover, setClickedDiscover] = useState(false);
+    const [clickedFollowing, setClickedFollowing] = useState(false);
+    const [clickedMyPosts, setClickedMyPosts] = useState(false);
 
     const handleMyPosts = () => {
+        setClickedDiscover(true);
+        setClickedMyPosts(false);
+        setClickedFollowing(true);
         setDiscover(false);
         setMyPosts(true);
     }
 
     const handleDiscover = () => {
+        setClickedDiscover(false);
+        setClickedMyPosts(true);
+        setClickedFollowing(true);
         setDiscover(true);
         setMyPosts(false);
+    }
+
+    const handleFollowing = () => {
+        setClickedDiscover(true);
+        setClickedMyPosts(true);
+        setClickedFollowing(false);
     }
 
   return (
@@ -26,9 +42,9 @@ const LeftSideNav = ({ user, setMyPosts, setDiscover }) => {
                 </div>
             </section>
             <section className="navButtons">
-                <button onClick={handleDiscover}>Discover</button>
-                <button>Following</button>
-                <button onClick={handleMyPosts}>My Posts</button>
+                <button onClick={handleDiscover}><ShinyText text="Discover" disabled={clickedDiscover} speed={3} className='custom-class' /></button>
+                <button onClick={handleFollowing}><ShinyText text="Following" disabled={clickedFollowing} speed={3} className='custom-class' /></button>
+                <button onClick={handleMyPosts}><ShinyText text="My Posts" disabled={clickedMyPosts} speed={3} className='custom-class' /></button>
                 <Link to={`/profile`}><button>Settings</button></Link>
             </section>
         </section>
